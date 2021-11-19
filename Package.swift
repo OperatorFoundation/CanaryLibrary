2eb2725213636083d1a81466a8caca84f0d5116d
consuelita <adelita@operatorfoundation.org>
2021-11-19 14:32:19 -0600

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         .package(url: "https://github.com/apple/swift-log.git", from: "1.4.2"),
         .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.0.1"),
-        .package(url: "https://github.com/OperatorFoundation/AdversaryLabClientSwift", from: "0.3.5"),
+        .package(url: "https://github.com/OperatorFoundation/AdversaryLabClientSwift", from: "0.3.10"),
         .package(url: "https://github.com/OperatorFoundation/Gardener.git", from: "0.0.48"),
         .package(url: "https://github.com/OperatorFoundation/ReplicantSwiftClient.git", from: "1.0.7"),
         .package(url: "https://github.com/OperatorFoundation/ShadowSwift.git", from: "2.1.1"),
