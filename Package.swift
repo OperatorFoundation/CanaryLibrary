2708ebec7df94a89fc744834e78d43f7f2e30242
consuelita <adelita@operatorfoundation.org>
2021-12-14 12:26:56 -0600

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -22,7 +22,7 @@ let package = Package(
         .package(url: "https://github.com/OperatorFoundation/ShadowSwift.git", from: "2.1.1"),
         .package(url: "https://github.com/OperatorFoundation/SwiftHexTools.git", from: "1.2.5"),
         .package(url: "https://github.com/OperatorFoundation/swift-netutils.git", from: "4.3.0"),
-        .package(url: "https://github.com/OperatorFoundation/Transmission.git", from: "1.2.4")
+        .package(url: "https://github.com/OperatorFoundation/Transmission.git", branch: "main")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
