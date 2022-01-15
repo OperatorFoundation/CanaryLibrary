a96741761064ee31bbe00262c276285438c5bdb5
consuelita <adelita@operatorfoundation.org>
2022-01-14 18:21:54 -0600

    Update Canary.swift

--- Sources/Canary/Canary.swift
+++ Sources/Canary/Canary.swift
@@ -6,7 +6,7 @@ public class Canary
 {
     private var chirp: CanaryTest
 
-    public required init(serverIP: String, configPath: String, savePath: String? = nil, logger: Logger, timesToRun: Int = 1, interface: String? = nil, debugPrints: Bool = false, runWebTests: Bool = false)
+    public required init(configPath: String, savePath: String? = nil, logger: Logger, timesToRun: Int = 1, interface: String? = nil, debugPrints: Bool = false, runWebTests: Bool = false)
     {
         uiLogger = logger
         chirp = CanaryTest(configDirPath: configPath, savePath: savePath, testCount: timesToRun, interface: interface, debugPrints: debugPrints, runWebTests: runWebTests)
